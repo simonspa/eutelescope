e7d6771569e7467c452d876877fc956e32975425
Simon Spannagel <simon.spannagel@desy.de>
2015-06-25 14:14:03 +0200

    CMSPixel analysis: for large tilt angles we have to increase the matchingn cut in X to get stable alignment. Now scalaling with 6.25um/deg startinng from 35deg tilt. This results in:  ~150um cut at 35deg tilt  ~400um cut at 70deg tilt

--- src/EUTelAnalysisCMSPixel.cc
+++ src/EUTelAnalysisCMSPixel.cc
@@ -307,6 +307,17 @@ void EUTelAnalysisCMSPixel::processRunHeader( LCRunHeader* runHeader) {
   m_millefilename = name.str();
   mille = new gbl::MilleBinary( m_millefilename.c_str() );
 
+
+  // Scale cutx with the expected resolution (depending on tilt angle)
+  // should always be around 10sigma
+  // do cutx = 0.15mm @ optimal res, up to 30deg tilt
+  //    cutx = 0.4mm @ high angles 75deg tilt
+  if(_DUTtilt > 35) {
+    _cutx += 0.00625*(_DUTtilt-35);
+    streamlog_out(MESSAGE0) << "Set matching cut X to " << _cutx << "mm" << std::endl;
+  }
+
+
   const std::string detectorName = runHeader->getDetectorName();
   const std::string detectorDescription = runHeader->getDescription();
 
